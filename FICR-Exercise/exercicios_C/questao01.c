fb74346c4e8c4e4d958bd68aff245dd713c614ca
MatheusMedeirosGS <matheustecworkout@outlook.com>
2025-10-30 15:54:30 -0300

    Add files via upload

--- FICR-Exercise/exercicios_C/questao01.c
+++ FICR-Exercise/exercicios_C/questao01.c
@@ -1,19 +1,22 @@
-//Questao 1: Faca um algoritmo que recebe dois valores inteiros e mostra o resultado da multiplicacao.
+/*Questao 1: Faca um algoritmo que recebe dois valores inteiros e mostra o resultado da multiplicacao.*/
 
 #include <stdio.h>
-main ()
-{		
-    // Variaveis 
+#include <locale.h>
+int main ()
+{	
+    /*Portugues*/
+    setlocale(LC_ALL,"");
+    
+    /*Variaveis*/
 	int a, b, result;
 	
-	//Entrada de dados
+	/*Entrada de dados*/
 	printf("Digite dois valores inteiros: ");
 	scanf("%i %i", &a, &b);
 	
-	//Saida de dados
-	result = a * b;	
-	printf("O resultado da multiplicacao entre os dois valores e: %i\n", result);
+	/*Saida de dados*/
+	result = a*b;	
+	printf("O resultado da multiplicacao entre os dois valores é: %i\n", result);
 	 		
-	system("pause"); 		
 	return 0;
 }
