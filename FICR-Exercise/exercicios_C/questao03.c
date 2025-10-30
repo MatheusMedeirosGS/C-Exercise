5255e50773f9dfd15727961aa8b619ffdc9dd7c1
MatheusMedeirosGS <matheustecworkout@outlook.com>
2025-10-30 16:29:47 -0300

    Add files via upload

--- FICR-Exercise/exercicios_C/questao03.c
+++ FICR-Exercise/exercicios_C/questao03.c
@@ -1,15 +1,18 @@
-//Questao 3: Fazer um algoritmo que obtenha dois numeros inteiros, x e y, mostre o quociente eo resto da divisao inteira entre eles.
+/*Questao 3: Fazer um algoritmo que obtenha dois numeros inteiros, x e y, mostre o quociente e o resto da divisao inteira entre eles.*/
+
 #include <stdio.h>
+#include <locale.h>
 
 int main()
 {	
+    setlocale (LC_ALL,"");
+    
 	int x, y;
 	printf("Digite dois valores inteiros (x y): ");
 	scanf("%i %i", &x, &y);
 
-	printf("O quociente e: %i\n", x / y);
-	printf("E o resto e: %i\n", x % y);
-
-	system("pause");
+	printf("O quociente é: %i\n", x / y);
+	printf("E o resto é: %i\n", x % y);
+	
 	return 0;
 }
