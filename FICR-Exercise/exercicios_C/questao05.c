1d34f602514d4fc055cc401217fb0962bf006ce6
MatheusMedeirosGS <matheustecworkout@outlook.com>
2025-10-31 15:31:08 -0300

    Add files via upload

--- FICR-Exercise/exercicios_C/questao05.c
+++ FICR-Exercise/exercicios_C/questao05.c
@@ -1,14 +1,17 @@
-//Questao 5: Fazer um algoritmo que converta centimetros para polegadas (obs.: 1pol = 2.54cm).
+/*Questao 5: Fazer um algoritmo que converta centimetros para polegadas (obs.: 1pol = 2.54cm).*/
+
 #include <stdio.h>
+#include <locale.h>
 
 int main()
-{	
+{
+    setlocale (LC_ALL,"");
+    
 	float cm;
 	printf("Digite o valor em centimetros: ");
 	scanf("%f", &cm);
 
-	printf("Em polegadas, o valor e: %.2fpol\n", cm / 2.54);
+	printf("Em polegadas, o valor é: %.2fpol\n", cm / 2.54);
 
-	system("pause");
 	return 0;
 }
