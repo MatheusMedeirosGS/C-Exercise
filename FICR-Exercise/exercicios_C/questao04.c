557580fec1549d5b5add16119e9dc4aeb3389fd4
MatheusMedeirosGS <matheustecworkout@outlook.com>
2025-10-31 15:26:25 -0300

    Add files via upload

--- FICR-Exercise/exercicios_C/questao04.c
+++ FICR-Exercise/exercicios_C/questao04.c
@@ -1,20 +1,22 @@
-//Questao 4: Fazer um algoritmo que calcule e mostre:
-//a) A soma entre dois numero:
-//b) O produto entre eles:
-//O quociente entre eles:
+/*Questao 4: Fazer um algoritmo que calcule e mostre:
+/a) A soma entre dois numero:
+/b) O produto entre eles:
+/O quociente entre eles:*/
 
 #include <stdio.h>
+#include <locale.h>
 
 int main()
 {	
+    setlocale (LC_ALL,"");
+    
 	float x, y;
-	printf("Digite dois numeros: ");
+	printf("Digite dois números: ");
 	scanf("%f %f", &x, &y);
 
 	printf("Soma: %.2f\n", x + y);
 	printf("Produto: %.2f\n", x * y);
 	printf("Quociente: %.2f\n", x / y);
 
-	system("pause");
 	return 0;
 }
