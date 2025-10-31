ca9eca6453747c3c7d9eecdc18a750e3261c457a
MatheusMedeirosGS <matheustecworkout@outlook.com>
2025-10-31 15:38:22 -0300

    Add files via upload

--- FICR-Exercise/exercicios_C/questao07.c
+++ FICR-Exercise/exercicios_C/questao07.c
@@ -1,21 +1,23 @@
-//Questao 7: Fazer um algoritmo que calcule e mostre a area e o volume de um cilindro.
-//(A = 2pr(h + r), V = pr2h)
+/*Questao 7: Fazer um algoritmo que calcule e mostre a area e o volume de um cilindro.
+/(A = 2pr(h + r), V = pr2h)*/
 
 #include <stdio.h>
+#include <locale.h>
 #define PI 3.14159
 
 int main()
 {	
+    setlocale (LC_ALL, "");
+    
 	float r, h, a, v;
 	printf("Digite o raio e a altura do cilindro: ");
 	scanf("%f %f", &r, &h);
 
-	a = 2 * PI * r * (h + r);
-	v = PI * r * r * h;
+	a = 2*PI*r*(h+r);
+	v = PI*r*r*h;
 
-	printf("Area: %.2f\n", a);
+	printf("Área: %.2f\n", a);
 	printf("Volume: %.2f\n", v);
 
-	system("pause");
-	return 0;
+    return 0;
 }
