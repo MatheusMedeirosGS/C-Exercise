8d192155ea8d91fcbf74be41bc2ea72d3a0bed9f
MatheusMedeirosGS <matheustecworkout@outlook.com>
2025-10-31 15:34:24 -0300

    Add files via upload

--- FICR-Exercise/exercicios_C/questao06.c
+++ FICR-Exercise/exercicios_C/questao06.c
@@ -1,17 +1,20 @@
-//Questao 6 Faca um algoritmo que recebe o valor do raio de um circulo e apresenta o valor da area desse circulo.
+/*Questao 6 Faca um algoritmo que recebe o valor do raio de um circulo e apresenta o valor da area desse circulo.*/
 
 #include <stdio.h>
-#define PI 3.14159
+#include <locale.h>
 #include <math.h>
+#define PI 3.14159
+
 
 int main()
 {	
+    setlocale (LC_ALL,"");
+    
 	float radius;
 	printf("Digite o raio do circulo: ");
 	scanf("%f", &radius);
 
-	printf("Area do circulo e: %.2fcm\n", PI * radius * radius);
+	printf("Área do circulo é: %.2fcm\n", PI*radius*radius);
 
-	system("pause");
 	return 0;
 }
