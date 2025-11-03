e5f6194d4aeb93f658894f60843d220cda6a75fb
MatheusMedeirosGS <matheustecworkout@outlook.com>
2025-11-03 16:48:20 -0300

    Add files via upload

--- FICR-Exercise/exercicios_C/questao14.c
+++ FICR-Exercise/exercicios_C/questao14.c
@@ -1,28 +1,32 @@
-//Questao 14: Faca um algoritmo que recebe um valor inteiro e mostra se o valor e zero, positivo ou negativo.
+/*Questao 14: Faca um algoritmo que recebe um valor inteiro e mostra se o valor e zero, positivo ou negativo.*/
 
 #include <stdio.h>
 #include <locale.h>
 
 int main()
 {
+    setlocale (LC_ALL,"");
+    
 	int num;
 
 	printf("Digite um valor inteiro: ");
-	scanf("%i", &num);
+	if (scanf("%i", &num) != 1) { 
+        printf("Erro: A entrada não é um número inteiro válido.\n");
+        return 1;
+	}
 
 	if (num > 0)
 	{
-		printf("O numero %i e positivo.\n", num);
+		printf("O número %i é positivo.\n", num);
 	}
 	else if (num < 0)
 	{
-		printf("O numero %i e negativo.\n", num);
+		printf("O número %i é negativo.\n", num);
 	}
 	else
 	{
-		printf("O valor informado e zero.\n");
+		printf("O valor informado é zero.\n");
 	}
 
-	system("pause");
 	return 0;
 }
