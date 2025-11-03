6a19b95f77cf6e9bdb618b363734f257c724c9eb
MatheusMedeirosGS <matheustecworkout@outlook.com>
2025-11-03 15:26:50 -0300

    Add files via upload

--- FICR-Exercise/exercicios_C/questao10.c
+++ FICR-Exercise/exercicios_C/questao10.c
@@ -1,7 +1,7 @@
-//Questao 10: Faca um algoritmo que mostra a quantidade de cedulas de um caixa eletronico de
-//um banco. O algoritmo recebe como entrada o valor inteiro a ser sacado pelo cliente, em seguida
-//apresente a quantidade de cada cedula que o cliente devera receber, de forma que a quantidade
-//de cedulas seja o menor possivel.
+/*Questao 10: Faca um algoritmo que mostra a quantidade de cedulas de um caixa eletronico de
+um banco. O algoritmo recebe como entrada o valor inteiro a ser sacado pelo cliente, em seguida
+apresente a quantidade de cada cedula que o cliente devera receber, de forma que a quantidade
+de cedulas seja o menor possivel.*/
 
 #include <stdio.h>
 #include <locale.h>
@@ -9,45 +9,35 @@
 
 int main()
 {
+    setlocale (LC_ALL,"");
+    
 	int valor_int;
 
 	printf("Digite o valor do saque: ");
 	scanf("%i", &valor_int);
 
-	printf("Valor a ser sacado: R$ %d,00\n", valor_int);
-	printf("Quantidade de cedulas que voce recebera: \n");
+	printf("\nValor a ser sacado: R$ %d,00\n", valor_int);
+	printf("Quantidade de cédulas que você receberá: \n");
 
-	int valor_restante = valor_int;
-	int quant;
+    printf("%i Cédula(s) de R$ 200 \n", valor_int / 200);
+	valor_int %= 200;
 
-	quant = valor_restante / 200;
-	printf("%i Cedula(s) de R$ 200 \n", quant);
-	valor_restante %= 200;
+	printf("%i Cédula(s) de R$ 100 \n", valor_int / 100);
+    valor_int %= 100;
 
-	quant = valor_restante / 100;
-	printf("%i Cedula(s) de R$ 100 \n", quant);
-	valor_restante %= 100;
+	printf("%i Cédula(s) de R$ 50 \n", valor_int / 50);
+    valor_int %= 50;
 
-	quant= valor_restante / 50;
-	printf("%i Cedula(s) de R$ 50 \n", quant);
-	valor_restante %= 50;
+	printf("%i Cédula(s) de R$ 20 \n", valor_int / 20);
+    valor_int %= 20;
 
-	quant= valor_restante / 20;
-	printf("%i Cedula(s) de R$ 20 \n", quant);
-	valor_restante %= 20;
+	printf("%i Cédula(s) de R$ 10 \n", valor_int / 10);
+    valor_int %= 10;
 
-	quant = valor_restante / 10;
-	printf("%i Cedula(s) de R$ 10 \n", quant);
-	valor_restante %= 10;
+	printf("%i Cédula(s) de R$ 5 \n", valor_int / 5);
+    valor_int %= 5;
 
-	quant = valor_restante / 5;
-	printf("%i Cedula(s) de R$ 5 \n", quant);
-	valor_restante %= 5;
+    printf("%i Cédula(s) de R$ 2 \n", valor_int / 2);
 
-	quant = valor_restante / 2;
-	printf("%i Cedula(s) de R$ 2 \n", quant);
-	valor_restante %= 2;
-
-	system("pause");
 	return 0;
 }
